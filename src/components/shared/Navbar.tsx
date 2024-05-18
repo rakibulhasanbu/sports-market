406b376a4b72ee37bd80562926cd6bb6c1a055fe
rakibulhasanbu <rakibulhasanbu@gmail.com>
2024-05-18 13:06:57 +0600

    updated and fix issues

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,10 +2,23 @@ import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../redux/hook";
 import { toast } from "react-toastify";
 import { logOut, useCurrentToken } from "../../redux/features/auth/authSlice";
+import { useState } from "react";
+import { GrMenu } from "react-icons/gr";
+import { Drawer } from "antd";
+import Sidebar from "./Sidebar";
 
 function Navbar() {
   const token = useAppSelector(useCurrentToken);
   const dispatch = useAppDispatch();
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const showDrawer = () => {
+    setOpenDrawer(true);
+  };
+
+  const onClose = () => {
+    setOpenDrawer(false);
+  };
 
   const handleLogout = () => {
     dispatch(logOut());
@@ -14,16 +27,17 @@ function Navbar() {
 
   const menuItems = (
     <>
-      <li>
-        <img src="/favicon.png" alt="" />
-        <Link className="text-2xl font-semibold text-primary" to="/">Sports Market</Link>
+      <li className="flex items-center gap-2">
+        <img src="/favicon.png" alt="" className="size-8 lg:size-11" />
+        <Link className="lg:text-2xl font-semibold text-primary" to="/">Sports Market</Link>
       </li>
+
       {token ? (
-        <li>
+        <li className="hidden lg:block">
           <button className="roundedBtn bg-red/80 hover:bg-red" onClick={handleLogout}>Logout</button>
         </li>
       ) : (
-        <li>
+        <li className="hidden lg:block">
           <Link className="roundedBtn bg-primary/80 hover:bg-primary" to="/login">Login</Link>
         </li>
       )}
@@ -31,32 +45,28 @@ function Navbar() {
   );
 
   return (
-    <div className="w-full flex justify-between">
+    <div className="w-full flex justify-between px-4 md:px-12 pt-2.5">
 
-      <div className="hidden lg:flex w-full px-12 pt-2.5">
-        <ul className="flex items-center justify-between w-full  gap-6 p-0">{menuItems}</ul>
-      </div>
+      <ul className="flex items-center justify-between w-full  gap-6 p-0">{menuItems}</ul>
 
-      <label
-        htmlFor="dashboard-drawer"
-        tabIndex={2}
-        className="btn btn-ghost lg:hidden"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16"
-          />
-        </svg>
-      </label>
+      <>
+        <div onClick={showDrawer} className='lg:hidden border rounded border-gray-200 p-1 text-textDark text-2xl'>
+          <GrMenu />
+        </div>
+        <Drawer width={"70%"} placement="left" onClose={onClose} open={openDrawer}>
+          <Sidebar />
+
+          {token ? (
+
+            <button className="roundedBtn bg-red/80 hover:bg-red pt-4" onClick={handleLogout}>Logout</button>
+
+          ) : (
+
+            <Link className="roundedBtn bg-primary/80 hover:bg-primary pt-4" to="/login">Login</Link>
+
+          )}
+        </Drawer>
+      </>
     </div>
   );
 }
